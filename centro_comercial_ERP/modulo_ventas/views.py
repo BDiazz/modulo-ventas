02d0aa88e4fa07f8323a042eb14a910b14314186
“Bryan <dv21005@ues.edu.sv>
2024-05-25 23:50:40 -0600

    agregue el template de barra y registrarVenta además del directorio static y su conexion en settings

--- centro_comercial_ERP/modulo_ventas/views.py
+++ centro_comercial_ERP/modulo_ventas/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 import datetime 
 # Create your views here.
+
+def registrarVenta(request):
+    fecha_actual = datetime.datetime.now
+    return render(request,'registrarVenta.html', {"fecha": fecha_actual,})
