6bf0206393ff3acac517e9837ce2a80566dead2d
Guillermo <guillermoardonalfaro@gmail.com>
2024-06-01 23:31:03 -0600

    subiendo las plantillas de modificar/eliminar, consultar y registrar servicios

--- centro_comercial_ERP/modulo_ventas/views.py
+++ centro_comercial_ERP/modulo_ventas/views.py
@@ -8,26 +8,25 @@ from django.db.models import Sum
 from django.utils import timezone
 from datetime import timedelta
 from django.contrib.auth.decorators import login_required
-from django import forms
-from django.contrib.auth import authenticate
-from django.contrib.auth.hashers import check_password
 # Create your views here.
 
-
+@login_required
 def registrarVenta(request):
     clientesObtenidos = Cliente.objects.all()
     fecha_actual = datetime.datetime.now 
     return render(request,'registrarVenta.html', {"fecha": fecha_actual, "clientes": clientesObtenidos,})
 
+@login_required
 def modificarVenta(request):
     return render(request, "modificarEliminarVenta.html")
 
+@login_required
 def eliminarVenta(request, identificador):
     venta = Venta.objects.get(id=identificador)
     venta.delete()
     return redirect("/consultarVentas")
 
-
+@login_required
 def consultar_ventas(request):
     filtro = request.GET.get('filtro', 'todas')
 
@@ -51,38 +50,21 @@ def consultar_ventas(request):
 
     return render(request, 'consultarVentas.html', {'ventas': ventas, 'total_ventas': total_ventas})
 
+@login_required
 def modificarVenta(request):
     return render(request, "modificarEliminarVenta.html")
 
 
-   
-
+@login_required
 def generar_reporte_de_ventas(request):
     return render(request, 'generarReporteDeVentas.html')  
 
-def vista_inicio_sesion(request):
-    class FormularioInicioSesion(forms.Form):
-        nombre = forms.CharField(label="Nombre de usuario")
-        contraseña = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
 
-    if request.method == 'POST':
-        formulario = FormularioInicioSesion(request.POST)
-        if formulario.is_valid():
-            nombre = formulario.cleaned_data['nombre']
-            contraseña = formulario.cleaned_data['contraseña']
-            usuario = authenticate(request, username=nombre, password=contraseña)
-            if usuario is not None:
-                # Autenticación exitosa
-                # Guardar el usuario en la sesión
-                request.session['usuario_id'] = usuario.id
-                return redirect('consultar_ventas')
-            else:
-                # Autenticación fallida
-                mensaje_error = "Nombre de usuario o contraseña incorrectos."
-        else:
-            # Formulario no válido
-            mensaje_error = "Formulario inválido. Por favor, verifica los datos."
-    else:
-        formulario = FormularioInicioSesion()
-        mensaje_error = None
-    return render(request, 'inicio_sesion.html', {'formulario': formulario, 'mensaje_error': mensaje_error})
+def consultarServicios(request):
+    return render(request, 'consultarServicios.html')
+
+def regristarServicio(request):
+    return render(request, 'registrarServicio.html')
+    
+def modificarEliminarServicio(request):
+    return render(request, 'modificarEliminarServicio.html')
